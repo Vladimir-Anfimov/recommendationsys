2a1e04895b9967d96d51670e0dbff3544f57c88b
SamuelLazurca <samilazurca15@gmail.com>
2023-05-28 16:32:43 +0300

    explorer

--- src/main/java/org/recsys/controllers/ExplorerController.java
+++ src/main/java/org/recsys/controllers/ExplorerController.java
@@ -1,14 +1,27 @@
 package org.recsys.controllers;
 
+import org.recsys.infrastucture.entities.Product;
+import org.recsys.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class ExplorerController {
 
+    @Autowired
+    ProductRepository productRepository;
+
     @GetMapping("/explorer")
-    public String explorer(Model model) {
+    public String explorer(Model model)
+    {
+
+        List<Product> recommendedProducts = productRepository.getRecommendedProducts(5);
+        model.addAttribute("recommendedProducts", recommendedProducts);
+
         return "explorer";
     }
 }
